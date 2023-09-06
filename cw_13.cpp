57326514f6fa106c7f712fbebaf7d3f304b23821
Timofei <tictacnumt@gmail.com>
2023-09-06 20:07:19 +0300

    improved cw_13

--- cw_13.cpp
+++ cw_13.cpp
@@ -1,16 +1,15 @@
-#include <iostream>
 #include <vector>
 
 class SqInRect {
   public:
- // void get_g() {std::cout << 'g';};
   static std::vector<int> sqInRect(int lng, int wdth) {
     std::vector<int> answ;
     if (lng == wdth) {
         return answ;
     }
-    while ((lng > 1) || (wdth > 1)) {
-        int min;
+    int min;
+    while (((lng > 1) || (wdth > 1)) && (lng != 0) && (wdth != 0)) {
+
         if (lng < wdth) {
             wdth -= lng;
             min = lng;
@@ -21,25 +20,8 @@ class SqInRect {
         }
         answ.push_back(min);
     }
-    answ.push_back(1);
+    if (min == 1)
+      answ.push_back(1);
     return (answ);
     };
 };
-/*
-std::vector<int> sqInRect(int lng, int wdth) {
-    std::vector<int> answ;
-    while ((lng > 1) || (wdth > 1)) {
-        int min = ( (lng < wdth) ? lng : wdth);
-        lng -= min;
-        wdth -= min;
-        answ.push_back(min);
-    }
-    return (answ);
-}
-*/
-int main () {
-    SqInRect obj;
- //   obj.get_g();
-    obj.sqInRect(5, 3);
-    return (0);
-}
